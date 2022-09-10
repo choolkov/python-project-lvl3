c9a591397b66e7cc71d91b7126f5d713bdcaf08a
choolkov <s.v.chulkov@ya.ru>
2022-09-10 18:17:23 +0300

    add exit codes

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,8 +1,16 @@
 """Page loader script."""
 import argparse
 import pathlib
+import sys
 
 from page_loader.loader import DEFAULT_PATH, download
+from page_loader.logging import errors_logger
+from requests.exceptions import RequestException
+
+OK_CODE = 0
+REQUESTS_ERROR_CODE = 1
+ACCESS_ERROR_CODE = 2
+UNKNOWN_ERROR_CODE = 3
 
 parser = argparse.ArgumentParser(
     description='Loads the HTML page.',
@@ -20,9 +28,44 @@ parser.add_argument('url', help='page URL', type=str)
 args = parser.parse_args()
 
 
+def handle_exception(exception: Exception, message: str, code: int):
+    """Exception handler.
+
+    Args:
+        exception: raised exception
+        message: message for user
+        code: exit code
+    """
+    errors_logger.error(exception)
+    print(message)
+    sys.exit(code)
+
+
 def main():
     """Run download and print full path to downloaded file."""
-    print(download(args.url, args.output))
+    try:
+        print(download(args.url, args.output))
+
+    except RequestException as exception:
+        handle_exception(
+            exception,
+            'Unable to download content from {0}'.format(args.url),
+            REQUESTS_ERROR_CODE,
+        )
+
+    except PermissionError as exception:
+        handle_exception(
+            exception,
+            'Access denied to output directory {0}'.format(args.output),
+            ACCESS_ERROR_CODE,
+        )
+
+    except Exception as exception:
+        handle_exception(
+            exception,
+            'Unknown error',
+            UNKNOWN_ERROR_CODE,
+        )
 
 
 if __name__ == '__main__':
